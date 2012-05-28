5dd91ef4c09fe4825c8087fa74b68b0356235595
Jessica Kerr <jessica@gmail.com>
2012-05-28 08:11:25 -0700

    Use one session; close db on application terminate

--- src/com/jessitron/telgame/TelephoneGameApplication.java
+++ src/com/jessitron/telgame/TelephoneGameApplication.java
@@ -4,20 +4,29 @@ import com.jessitron.telephonegame.dao.DaoMaster;
 import com.jessitron.telephonegame.dao.DaoSession;
 
 import android.app.Application;
+import android.database.sqlite.SQLiteDatabase;
 
 public class TelephoneGameApplication extends Application {
 
-    private DaoMaster daoMaster;
+    private DaoSession session;
+    private SQLiteDatabase db;
 
 
     @Override
     public void onCreate() {
-        daoMaster = new DaoMaster(new DaoMaster.DevOpenHelper(this, "TELEPHONEGAME", null).getWritableDatabase());
+        db = new DaoMaster.DevOpenHelper(this, "TELEPHONEGAME", null).getWritableDatabase();
+        DaoMaster daoMaster = new DaoMaster(db);
+        session = daoMaster.newSession();
         super.onCreate();
     }
 
     public DaoSession getDBSession() {
-        return daoMaster.newSession();
+        return session;
     }
 
+    @Override
+    public void onTerminate() {
+        db.close();
+        super.onTerminate(); 
+    }
 }
